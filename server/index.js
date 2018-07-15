91e5277655c967e5ea596bad895795ed310a0d7f
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-15 15:39:38 -0700

    Structure server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const parser = require("body-parser");
+
 const app = express();
 
-// app.get("/", (req, res) => res.send("Hello World!"));
+app.use(parser.json());
+
 app.use(express.static("client/dist"));
+
+// GET
+app.get("/gameadata", (req, res) => res.send("Hello World!"));
+
+// POST
+app.post("/gameadata", (req, res) => res.send(req.body));
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
