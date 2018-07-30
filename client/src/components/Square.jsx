9cc9f77980eb15776093d9d18198856d637e1e85
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-07-30 09:41:25 -0700

    Implement king textures

--- client/src/components/Square.jsx
+++ client/src/components/Square.jsx
@@ -17,6 +17,14 @@ class Square extends React.Component {
       this.setState({ colorDisplayed: "red-selected" });
     } else if (this.props.gameState[this.props.row][this.props.index] === "B") {
       this.setState({ colorDisplayed: "black-selected" });
+    } else if (
+      this.props.gameState[this.props.row][this.props.index] === "RK"
+    ) {
+      this.setState({ colorDisplayed: "red-king" });
+    } else if (
+      this.props.gameState[this.props.row][this.props.index] === "BK"
+    ) {
+      this.setState({ colorDisplayed: "black-king" });
     } else {
       this.setState({ colorDisplayed: null });
     }
@@ -47,12 +55,14 @@ class Square extends React.Component {
   }
 
   render() {
-    let RedGamePiece = <div className="gamepiece-red animated flipInX" />;
-    let BlackGamePiece = <div className="gamepiece-black animated flipInX" />;
-    let RedGamePieceSelected = (
+    const redGamePiece = <div className="gamepiece-red animated flipInX" />;
+    const blackGamePiece = <div className="gamepiece-black animated flipInX" />;
+    const redKing = <div className="gamepiece-red-king animated flipInX" />;
+    const blackKing = <div className="gamepiece-black-king animated flipInX" />;
+    const redGamePieceSelected = (
       <div className="gamepiece-red animated infinite jello" />
     );
-    let BlackGamePieceSelected = (
+    const blackGamePieceSelected = (
       <div className="gamepiece-black animated infinite jello" />
     );
     return (
@@ -60,13 +70,15 @@ class Square extends React.Component {
         className="tile"
         onClick={() => this.props.handleClick(this.props.row, this.props.index)}
       >
-        {this.state.colorDisplayed === "red" ? RedGamePiece : null}
-        {this.state.colorDisplayed === "black" ? BlackGamePiece : null}
+        {this.state.colorDisplayed === "red" ? redGamePiece : null}
+        {this.state.colorDisplayed === "black" ? blackGamePiece : null}
+        {this.state.colorDisplayed === "red-king" ? redKing : null}
+        {this.state.colorDisplayed === "black-king" ? blackKing : null}
         {this.state.colorDisplayed === "red-selected"
-          ? RedGamePieceSelected
+          ? redGamePieceSelected
           : null}
         {this.state.colorDisplayed === "black-selected"
-          ? BlackGamePieceSelected
+          ? blackGamePieceSelected
           : null}
       </div>
     );
